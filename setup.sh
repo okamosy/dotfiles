f7a7bc09fa0ae9591c1305d29a1979b9adaae9b2
Seiji Okamoto <okamosy@gmail.com>
2024-06-28 20:20:32 +0100

    Update to use oh-my-post instead of oh-my-zsh

--- setup.sh
+++ setup.sh
@@ -10,25 +10,23 @@ sudo apt upgrade -y
 
 sudo apt install -y \
   build-essential \
+  gcc \
   stow \
   zsh
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 chsh -s $(which zsh)
 
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 NONINTERACTIVE=1 /home/linuxbrew/.linuxbrew/bin/brew install \
-  cilium-cli \
   fzf \
   helm \
-  hubble \
   jq \
   just \
   k9s \
   kubectl \
   kubectx \
+  oh-my-posh \
   zoxide
 
 $(/home/linuxbrew/.linuxbrew/bin/brew --prefix)/opt/fzf/install --all
